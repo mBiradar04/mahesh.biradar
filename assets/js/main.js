17f59fea264f344a79242f851fdc29926806e4a7
mBiradar04 <mahesh.biradar9702@gmail.com>
2025-01-18 16:09:47 +0530

    Add about and skills section

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,20 @@ if (selectTyped) {
         backDelay: 2000
     });
 }
+
+/**
+ * Animate the skills items on reveal
+ */
+let skillsAnimation = document.querySelectorAll('.skills-animation');
+skillsAnimation.forEach((item) => {
+    new Waypoint({
+        element: item,
+        offset: '80%',
+        handler: function (direction) {
+            let progress = item.querySelectorAll('.progress .progress-bar');
+            progress.forEach(el => {
+                el.style.width = el.getAttribute('aria-valuenow') + '%';
+            });
+        }
+    });
+});
