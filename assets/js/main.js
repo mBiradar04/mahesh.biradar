69e321e6c48611a53b89cb8c42d4e96b231f7aa3
mBiradar04 <mahesh.biradar9702@gmail.com>
2025-03-30 14:09:05 +0530

    Add Resume and project sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,3 +43,20 @@ skillsAnimation.forEach((item) => {
         }
     });
 });
+
+/**
+ * Init swiper sliders
+*/
+function initSwiper() {
+    document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
+        let config = JSON.parse(
+            swiperElement.querySelector(".swiper-config").innerHTML.trim()
+        );
+
+        if (swiperElement.classList.contains("swiper-tab")) {
+            initSwiperWithCustomPagination(swiperElement, config);
+        } else {
+            new Swiper(swiperElement, config);
+        }
+    });
+}
